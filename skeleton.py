05163dedfaddde56912d0e14cad476cb22d6abe8
sunain-s <sas@drsyed.org>
2025-11-08 18:23:10 +0000

    feat: main_connective() - returns the index and type of binary connective

--- skeleton.py
+++ skeleton.py
@@ -1,31 +1,71 @@
 
 MAX_CONSTANTS = 10
 
+#------------------------------------------------------------------------------------------------------------------------------:
+# Helper Functions
 
+def main_connective(fmla) -> tuple[int, str]:
+    depth = 0
+    i = 0
+    while i < len(fmla):
+        char = fmla[i]
+        if char == '(':
+            depth += 1
+        elif char == ')':
+            depth -= 1
+
+        # checking at highest level for the main connective
+        elif depth == 1:
+            if char == "&":
+                return i, "&"
+            elif char == "\\" and i + 1 < len(fmla) and fmla[i + 1] == "/":
+                return i, "\\/"
+            elif char == "-" and i + 1 < len(fmla) and fmla[i + 1] == ">":
+                return i, "->"
+        i += 1
+    return None, None
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------:
+# Testing Functions
+
+def test_main_connective():
+    assert main_connective('(P&Q)') == (2, '&')
+    assert main_connective('((P\\/Q)->R)') == (7, '->')
+    assert main_connective('(P->(Q&R))') == (2, '->')
+    assert main_connective('(P)') == (None, None)
+    assert main_connective('((P&Q)\\/ (R->S))') == (6, '\\/')
+    print("All tests passed.")
+
+test_main_connective()
+print("================================\n\n\n")
+
+#------------------------------------------------------------------------------------------------------------------------------:
+# Main Tableau Functions
 
 # Parse a formula, consult parseOutputs for return values.
-def parse(fmla):
+def parse(fmla) -> int:
     return 0
 
 # Return the LHS of a binary connective formula
-def lhs(fmla):
+def lhs(fmla) -> str:
     return ''
 
 # Return the connective symbol of a binary connective formula
-def con(fmla):
+def con(fmla) -> str:
     return ''
 
 # Return the RHS symbol of a binary connective formula
-def rhs(fmla):
+def rhs(fmla) -> str:
     return ''
 
-
 # You may choose to represent a theory as a set or a list
-def theory(fmla):#initialise a theory with a single formula in it
+def theory(fmla) -> list[str]:  # initialise a theory with a single formula in it
     return None
 
 #check for satisfiability
-def sat(tableau):
+def sat(tableau) -> int:
 #output 0 if not satisfiable, output 1 if satisfiable, output 2 if number of constants exceeds MAX_CONSTANTS
     return 0
 
